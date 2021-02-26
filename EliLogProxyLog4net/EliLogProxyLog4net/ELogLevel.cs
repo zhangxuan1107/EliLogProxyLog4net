59cdb748f3447c4828ad580015b8772bc431e3de
zhangxuan <zhangxuan1107@163.com>
2021-02-26 10:10:13 +0800

    将日志组件改得更适合引用

---
+++ EliLogProxyLog4net/EliLogProxyLog4net/ELogLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EliLogProxyLog4net
+{
+    public enum ELogLevel
+    {
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR
+    }
+}
--- EliLogProxyLog4net/EliLogProxyLog4net/LogProxy.cs
+++ EliLogProxyLog4net/EliLogProxyLog4net/LogProxy.cs
@@ -1,6 +1,9 @@
 ﻿using log4net;
+using log4net.Config;
+using log4net.Repository;
 using System;
 using System.Configuration;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,14 +12,41 @@ namespace EliLogProxyLog4net
 {
     public class LogProxy
     {
-        private static ILog _Log = LogManager.GetLogger("Performance");
+        static ILoggerRepository repository = LogManager.CreateRepository("NETCoreRepository");
+
+        private static ILog _Log = null;
+        //private static ILog _Log = LogManager.GetLogger("logerror");
         private static Action<string> _action;
         private static UdpClient _udpClient = new UdpClient();
-        private static string remUdpIp;
-        private static int remUdpPort;
-        public static void Info(string logStr)
+        /// <summary>
+        /// 是否已经初始化
+        /// </summary>
+        private static bool isInited = false;
+        /// <summary>
+        /// 调用前必须先初始化
+        /// </summary>
+        private static void Init()
         {
-            ProcessLog(logStr);
+            XmlConfigurator.Configure(repository, new FileInfo("config.xml"));
+            _Log = LogManager.GetLogger(repository.Name, "NETCorelog4net");
+        }
+        /// <summary>
+        /// 远程IP
+        /// 用于接受日志
+        /// </summary>
+        private static string remUdpIp;
+
+        /// <summary>
+        /// 远程端口
+        /// </summary>
+        private static int remUdpPort;
+        public static void Write(ELogLevel level, string logStr)
+        {
+            if (!isInited)
+            {
+                Init();
+            }
+            ProcessLog(level, logStr);
             if (_action != null)
             {
                 _action(logStr);
@@ -28,9 +58,31 @@ namespace EliLogProxyLog4net
             _action = action;
         }
 
-        private static void ProcessLog(string logStr)
+        private static void ProcessLog(ELogLevel level, string logStr)
         {
-            _Log.Info(logStr);//记录日志
+            switch (level)
+            {
+                case ELogLevel.DEBUG:
+                    _Log.Debug(logStr);
+                    break;
+                case ELogLevel.INFO:
+                    _Log.Info(logStr);
+                    break;
+                case ELogLevel.WARN:
+                    _Log.Warn(logStr);
+                    break;
+                case ELogLevel.ERROR:
+                    _Log.Error(logStr);
+                    break;
+                default:
+                    break;
+            }
+            SendMsg(logStr);
+        }
+
+
+        private static void SendMsg(string logStr)
+        {
             if (string.IsNullOrWhiteSpace(remUdpIp))
             {
                 remUdpIp = ConfigurationManager.AppSettings["remUdpIp"] == null ? "127.0.0.1" :
